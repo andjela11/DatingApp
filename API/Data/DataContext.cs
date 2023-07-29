f399494104651b21088bbf8c675889a05eb7a934
andjela11 <andjela1198@gmail.com>
2023-07-29 15:25:41 +0200

    added like functionality

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,27 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserLike>()
+                .HasKey(x => new {x.SourceUserId, x.TargetUserId});
+
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(s => s.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(s => s.LikedByUsers)
+                .HasForeignKey(s => s.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
